b8fba941583afce366a12bcbc2097dc79e95afe2
HyungsooLim <71387689+HyungsooLim@users.noreply.github.com>
2022-06-15 10:18:34 +0900

    Initial commit

---
+++ src/main/java/com/hs/warmupboard/WarmupboardApplication.java
@@ -0,0 +1,13 @@
+package com.hs.warmupboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmupboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmupboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hs/warmupboard/WarmupboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hs.warmupboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmupboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
