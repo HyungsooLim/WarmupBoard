e8921ed56540f96f30faaeefbb2469cbb4dade28
HyungsooLim <71387689+HyungsooLim@users.noreply.github.com>
2022-06-16 15:36:10 +0900

    success link axios

---
+++ src/main/java/com/hs/warmupboard/board/BoardVO.java
@@ -0,0 +1,12 @@
+package com.hs.warmupboard.board;
+
+//import lombok.Data;
+//
+//import javax.persistence.Entity;
+
+//@Data
+//@Entity(name = "board")
+//public class BoardVO {
+//
+//
+//}
--- src/main/java/com/hs/warmupboard/home/HomeController.java
+++ src/main/java/com/hs/warmupboard/home/HomeController.java
@@ -1,14 +1,18 @@
 package com.hs.warmupboard.home;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/") // RequestMapping URI 를 지정
+@CrossOrigin(value = "http://localhost:3000")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/") // http://localhost:8080/api/hello
     public String goHome() {
-        return "index";
+        return "axios test";
     }
 
 }
